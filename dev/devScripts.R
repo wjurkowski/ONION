632f6ba2b95eacf861abbddeaf4123881233f86b
cturek <cturek@enterprisesoftwaregroup.com>
2016-01-13 23:37:18 +0100

    Add method to set dev libraries before build. Future TODO on build process.

--- dev/devScripts.R
+++ dev/devScripts.R
@@ -1,29 +1,34 @@
+#Legend:
+#o - operative
+
 #o. Clear R console.
 cat("\014")
 
-#Run R CMD CHECK.
-install.packages('devtools')
-library('devtools')
-check(document = FALSE)
+#o. Load all libraries necessary to build package.
+setDeveloperEnvironment <- function() {
+    install.packages('igraph')
+    install.packages('pls')
+    install.packages('corrplot')
+    install.packages('yacca')
+    install.packages('FRCC')
+    install.packages('RCurl')
+    install.packages('testthat')
+    install.packages('httr')
+    install.packages('RJSONIO')
+    install.packages('XML')
+    install.packages('S4Vectors')
+    #install.packages('stats') not available anyweare.
+    source('https://bioconductor.org/biocLite.R')
+    biocLite('biomaRt')
+    biocLite('STRINGdb')
+    biocLite('BiocCheck')
+    biocLite('S4Vectors')
+    biocLite('ChemmineR')
+    biocLite('ChemmineOB')
+}
 
-#Create and Run tests.
-library(devtools)
-#It will ask you about test path creation to testthat.R
-#test()
+#TODO: Set up devtools to smart and fast builds.
 
-#Set proper work directory.
+#TODO: Continous delivery!
 
-
-
-#Analyze to understend lists.
-dane <- getChEBIIdsListOfOntologyParents(28842)
-dane
-
-library(ONION)
-ONION::mapReactome()
-cat("\014")
-
-
-#TODO bug with ChEBI names.
-l <- list(aaa=c("as as as"), aaa=c("aa bb cc"))
-l$aaa
+#TODO: Set up roxygen2 to autogeneration of manual.
