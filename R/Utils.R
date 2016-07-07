f08c4340d8b107a08d6582a93d0f6293eef6f6cd
Cezary Turek <cezary.turek@gmail.com>
2016-07-07 20:20:27 +0200

    Data frames as inputs instead of paths to files.

--- R/Utils.R
+++ R/Utils.R
@@ -1,7 +1,2 @@
 
 # TRUE helpers.
-readWithoutDuplicates <- function(filePath, header = TRUE) {
-    transcriptomics <- read.table(filePath, header = header, row.names = NULL);
-    transcriptomicsWithoutDuplicates <- data.frame(transcriptomics[!duplicated(transcriptomics[1]), ], row.names = 1)
-    transcriptomicsWithoutDuplicates
-}
